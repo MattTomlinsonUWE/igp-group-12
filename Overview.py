eefc04101ec1b25e40d3a7a6c0709f26b8967d9e
MattTomlinsonUWE <matthew3.tomlinson@live.uwe.ac.uk>
2025-04-29 13:53:00 +0100

    Add disclaimer

--- Overview.py
+++ Overview.py
@@ -22,6 +22,10 @@ st.set_page_config(
     layout="wide",
 )
 
+# Display the disclaimer
+with st.sidebar:
+    st.write("**Disclaimer:** The following information is provided for informational purposes only and is not investment advice. You should not make investments based on this advice and in teh event that you do, no liability will be accepted for any losses.")
+
 st.write("# Stock Market Seasonality Strategies")
 
 st.markdown(
